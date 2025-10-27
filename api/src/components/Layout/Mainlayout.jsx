ecd252a46be85cc33396d75ca0da7288b896f40d
Daniel-Clerkson <danielclerkson68@gmail.com>
2025-10-27 15:50:15 +0000

    Updates-2

--- api/src/components/Layout/Mainlayout.jsx
+++ api/src/components/Layout/Mainlayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar';
+import ScrollToTop from '../../ScrollToTop';
 
 const Mainlayout = () => {
   return (
     <div>
+      <ScrollToTop />
       <Navbar onOpenCart={() => setCartOpen(true)} />
         <Outlet />
     </div>
